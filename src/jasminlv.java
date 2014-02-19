3a619ecf4080448ace05eecd2ac4f3b30b6c35df
Alumno <alumno@localhost.localdomain>
2014-02-19 08:27:06 -0600

    Clase 19 febrero

---
+++ src/jasminlv.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+	public class jasminlv {
+
+	public static void main(String[] args) {
+		System.out.print("Ingrese un Numero");
+		Scanner volado=new Scanner (System.in);
+		string animal=volado.nextInt();
+		
+		switch (animal)
+		{
+			case 1:
+			System.out.println("Es un Uno");
+			break;
+		
+			case 2:
+				System.out.print("Es un Dos");
+				break;
+		
+		
+			case 3:
+				System.out.print("Es un Tres");
+				break;
+		
+		
+			default:
+				System.out.print("Es otro");
+				break;
+		}
+	}
+	}
+
